a237b16580279c3f633e93c1a97c902ad87ef5ea
Wander Luiz <wander.srv@gmail.com>
2021-07-28 18:11:26 -0300

    Criaçao da pasta dia-1-javascript-primeiros-passos, pastas exercicios 1 a 5 e respectivos arquivos script.js 1 a 5

---
+++ Fundamentos/Bloco-4-Intro-JS-Logica-Programacao/dia-1-javascript-primeiros-passos/exercicio-5/script.js
@@ -0,0 +1,18 @@
+let resultado = "lista";
+
+switch (resultado){
+  case "aprovada":
+    console.log("Parabens, você foi aprovado(a)!");
+  break;
+
+  case "lista":
+    console.log("Você está na lista de espera!");
+  break;
+
+  case "reprovada":
+    console.log("Você foi reprovado(a)!");
+  break;
+
+  default:
+    console.log("Não se aplica!");
+}
