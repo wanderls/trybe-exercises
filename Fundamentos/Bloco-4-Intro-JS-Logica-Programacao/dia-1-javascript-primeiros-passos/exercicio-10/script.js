36738662fb51c7d1dbeca446013be390f82b786f
Wander Luiz <87048899+wanderls@users.noreply.github.com>
2021-07-29 13:59:26 -0300

    Update script.js
    Modificação da linha 8 (else).

--- Fundamentos/Bloco-4-Intro-JS-Logica-Programacao/dia-1-javascript-primeiros-passos/exercicio-10/script.js
+++ Fundamentos/Bloco-4-Intro-JS-Logica-Programacao/dia-1-javascript-primeiros-passos/exercicio-10/script.js
@@ -1,12 +1,11 @@
 let custoCompra = 20;
 let custoVenda = 60;
 
-let lucroVenda = custoVenda - custoCompra + (custoCompra * 0,2);
-
 if (custoCompra < 0 || custoVenda < 0){
   console.log("Valores invalidos.");
 }
 else{
+  let lucroVenda = custoVenda - custoCompra + (custoCompra * 0.2);
   lucroMil = lucroVenda * 1000;
   console.log("Lucro unitario é: R$ " + lucroVenda + ". Lucro da venda de 1000 un é: R$ " + lucroMil);
 }
